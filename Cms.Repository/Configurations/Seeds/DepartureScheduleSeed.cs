9722dd44773622eb1853b7b1f4e1177fc2958508
duongbilly18012004@gmail.com <duongbilly18012004@gmail.com>
2026-07-01 19:26:17 +0700

    feat(all): refactor all

--- Cms.Repository/Configurations/Seeds/DepartureScheduleSeed.cs
+++ Cms.Repository/Configurations/Seeds/DepartureScheduleSeed.cs
@@ -9,7 +9,7 @@ public static class DepartureScheduleSeed
     {
         builder.HasData(
             new DepartureSchedule { Id = SeedIds.DepartureSummer, ServiceId = SeedIds.ServiceMain, StartTime = "2026-07-15", Code = "DEP-001", Price = "3500000/customer", AccommodationStandards = "3-star hotel", CreatedAt = SeedIds.CreatedAt, UpdatedAt = SeedIds.CreatedAt },
-            new DepartureSchedule { Id = SeedIds.DepartureAutumn, ServiceId = SeedIds.ServiceResort, StartTime = "2026-09-20", Code = "DEP-002", Price = "5200000/customer", AccommodationStandards = "4-star resort", CreatedAt = SeedIds.CreatedAt, UpdatedAt = SeedIds.CreatedAt },
-            new DepartureSchedule { Id = SeedIds.DeparturePrivate, ServiceId = SeedIds.ServicePrivate, StartTime = "On request", Code = "DEP-003", Price = "Quote on request", AccommodationStandards = "Boutique hotel", CreatedAt = SeedIds.CreatedAt, UpdatedAt = SeedIds.CreatedAt });
+            new DepartureSchedule { Id = SeedIds.DepartureAutumn, ServiceId = SeedIds.ServiceMain, StartTime = "2026-09-20", Code = "DEP-002", Price = "5200000/customer", AccommodationStandards = "4-star resort", CreatedAt = SeedIds.CreatedAt, UpdatedAt = SeedIds.CreatedAt },
+            new DepartureSchedule { Id = SeedIds.DeparturePrivate, ServiceId = SeedIds.ServiceMain, StartTime = "On request", Code = "DEP-003", Price = "Quote on request", AccommodationStandards = "Boutique hotel", CreatedAt = SeedIds.CreatedAt, UpdatedAt = SeedIds.CreatedAt });
     }
 }
---
+++ Cms.Repository/Migrations/20260701122102_FixServiceRelationshipSeeds.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cms.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixServiceRelationshipSeeds : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "DepartureSchedules",
+                keyColumn: "Id",
+                keyValue: new Guid("44444444-4444-4444-4444-444444444442"),
+                column: "ServiceId",
+                value: new Guid("11111111-1111-1111-1111-111111111111"));
+
+            migrationBuilder.UpdateData(
+                table: "DepartureSchedules",
+                keyColumn: "Id",
+                keyValue: new Guid("44444444-4444-4444-4444-444444444443"),
+                column: "ServiceId",
+                value: new Guid("11111111-1111-1111-1111-111111111111"));
+
+            migrationBuilder.UpdateData(
+                table: "ImportantInfors",
+                keyColumn: "Id",
+                keyValue: new Guid("55555555-5555-5555-5555-555555555552"),
+                column: "ServiceId",
+                value: new Guid("11111111-1111-1111-1111-111111111113"));
+
+            migrationBuilder.UpdateData(
+                table: "RoomCategories",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333331"),
+                column: "ServiceId",
+                value: new Guid("11111111-1111-1111-1111-111111111112"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "DepartureSchedules",
+                keyColumn: "Id",
+                keyValue: new Guid("44444444-4444-4444-4444-444444444442"),
+                column: "ServiceId",
+                value: new Guid("11111111-1111-1111-1111-111111111112"));
+
+            migrationBuilder.UpdateData(
+                table: "DepartureSchedules",
+                keyColumn: "Id",
+                keyValue: new Guid("44444444-4444-4444-4444-444444444443"),
+                column: "ServiceId",
+                value: new Guid("11111111-1111-1111-1111-111111111113"));
+
+            migrationBuilder.UpdateData(
+                table: "ImportantInfors",
+                keyColumn: "Id",
+                keyValue: new Guid("55555555-5555-5555-5555-555555555552"),
+                column: "ServiceId",
+                value: new Guid("11111111-1111-1111-1111-111111111112"));
+
+            migrationBuilder.UpdateData(
+                table: "RoomCategories",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333331"),
+                column: "ServiceId",
+                value: new Guid("11111111-1111-1111-1111-111111111111"));
+        }
+    }
+}
